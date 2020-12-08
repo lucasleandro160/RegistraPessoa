16726b9b7bf83e671ab09c0e06aab5baec55c532
lucasleandro160 <alkhyz23@gmail.com>
2020-12-08 13:50:07 -0300

    Login

---
+++ RegistraPessoa.Web/Controllers/UsuariosController.cs
@@ -0,0 +1,94 @@
+﻿using RegistraPessoa.Dados.Entity.Context;
+using RegistraPessoa.Web.Identity;
+using RegistraPessoa.Web.ViewModels.Usuarios;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistraPessoa.Web.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        [AllowAnonymous]
+        public ActionResult CriarUsuario()
+        {
+            return View();
+        }
+
+        // POST: Usuarios
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CriarUsuario(UsuarioViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var userStore = new UserStore<IdentityUser>(new PessoaIndentityDbContext());
+                var userManager = new UserManager<IdentityUser>(userStore);
+                var identityUser = new IdentityUser
+                {
+                    UserName = viewModel.Email,
+                    Email = viewModel.Email
+                };
+
+                IdentityResult resultado = userManager.Create(identityUser, viewModel.Senha);
+
+                if (resultado.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("erro_identity", resultado.Errors.First());
+                    return View(viewModel);
+                }
+            }
+            return View(viewModel);
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UsuarioViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var userStore = new UserStore<IdentityUser>(new PessoaIndentityDbContext());
+                var userManager = new UserManager<IdentityUser>(userStore);
+                var usuario = userManager.Find(viewModel.Email, viewModel.Senha);
+
+                if (usuario == null)
+                {
+                    ModelState.AddModelError("erro_identity", "Usuário ou senha incorretos!");
+                    return View(viewModel);
+                }
+
+                var authManager = HttpContext.GetOwinContext().Authentication;
+                var identity = userManager.CreateIdentity(usuario, DefaultAuthenticationTypes.ApplicationCookie);
+                authManager.SignIn(new Microsoft.Owin.Security.AuthenticationProperties
+                {
+                    IsPersistent = false
+                }, identity);
+
+                return RedirectToAction("Index", "Home");
+            }
+            return View(viewModel);
+        }
+
+        [Authorize]
+        public ActionResult Logoff()
+        {
+            var authManager = HttpContext.GetOwinContext().Authentication;
+            authManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ RegistraPessoa.Web/ViewModels/Usuarios/UsuarioViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistraPessoa.Web.ViewModels.Usuarios
+{
+    public class UsuarioViewModel
+    {
+        [Required(ErrorMessage = "O E-mail é obrigatório!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "A Senha é obrigatória!")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+    }
+}
